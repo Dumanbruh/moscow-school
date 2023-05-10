adcf1d84236dde0d07f7feeb671002d91866cb39
Duman <201605@astanait.edu.kz>
2023-05-10 14:43:06 +0600

    email logo change

--- utils/record.ts
+++ utils/record.ts
@@ -88,7 +88,7 @@ export const recordHTML = (code: number) => {
                       <td class="es-m-p0r" valign="top" align="center" style="padding:0;Margin:0;width:560px">
                       <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                         <tr>
-                          <td align="center" style="padding:0;Margin:0;padding-top:5px;padding-bottom:10px;font-size:0px"><a target="_blank" href="https://fizkult.moscow.sport" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#666666;font-size:14px"><img src="images/logo.svg" alt="Logo" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" title="Logo" height="59" class="adapt-img"></a></td>
+                          <td align="center" style="padding:0;Margin:0;padding-top:5px;padding-bottom:10px;font-size:0px"><a target="_blank" href="https://fizkult.moscow.sport" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#666666;font-size:14px"><img src="https://gdncnh.stripocdn.email/content/guids/CABINET_13abf1ff3d74c13d7c4f49be1ab02dd987828f46e2c7c0af2843e537ffa73d7a/images/group_431.png" alt="Logo" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" title="Logo" height="59" class="adapt-img"></a></td>
                         </tr>
                       </table></td>
                     </tr>
